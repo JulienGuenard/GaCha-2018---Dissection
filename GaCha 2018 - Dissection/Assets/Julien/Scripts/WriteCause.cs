c6744f2a1e99935d23129c5ad5e9ec025380b8c2
JulienGuenard <julienguenard01@gmail.com>
2018-04-11 01:35:34 +0200

    Bones collision au marteau à faire

---
+++ GaCha 2018 - Dissection/Assets/Julien/Scripts/WriteCause.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WriteCause : MonoBehaviour
+{
+  public InputField nameField;
+
+  private string charName;
+
+  public static WriteCause Instance;
+
+  // Use this for initialization
+  void Awake()
+  {
+    Instance = this;
+  }
+	
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.Return))
+      Debug.Log("a");
+  }
+
+  void StringUpdate()
+  {
+    charName = nameField.text;
+     
+
+    //   Debug.
+  }
+
+}
