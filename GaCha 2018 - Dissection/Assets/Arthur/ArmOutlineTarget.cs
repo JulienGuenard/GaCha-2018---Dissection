446003dd343fd41556ca211ac809f095944646d6
Unknown <oncheonche@ymail.com>
2018-04-10 16:04:22 +0200

    Outline

---
+++ GaCha 2018 - Dissection/Assets/Arthur/ArmOutlineTarget.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmOutlineTarget : MonoBehaviour {
+
+    public Material artere;
+    public Material artere_silhouette;
+    public Material organe;
+    public Material organe_silhouette;
+    public Material os;
+    public Material os_silhouette;
+    public Renderer materialRenderer;
+    public Material[] mats;
+    public GameObject lastTarget;
+
+    void Update()
+    {
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity))
+        {
+                activateOutline(hit);
+        }
+    }
+
+    void activateOutline(RaycastHit hit)
+    {
+        if (lastTarget != null)
+        {
+            if (lastTarget != hit.transform.gameObject)
+                disableOutline(lastTarget);
+        }
+        //        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+        //        Debug.Log(hit.transform.gameObject);
+        materialRenderer = hit.transform.GetComponent<Renderer>();
+        if (materialRenderer == null)
+        {
+            disableOutline(lastTarget);
+            return;
+        }
+        mats = materialRenderer.materials;
+
+        //Here, you have to assign with the exact order!
+        if (hit.transform.CompareTag("Artere") == true)
+        {
+            mats[0] = artere_silhouette;
+        }
+        //Here, you have to assign with the exact order!
+        else if (hit.transform.CompareTag("Organe") == true)
+        {
+            mats[0] = organe_silhouette;
+        }
+        //Here, you have to assign with the exact order!
+        else if (hit.transform.CompareTag("Os") == true)
+        {
+            mats[0] = os_silhouette;
+        }
+
+        materialRenderer.materials = mats;
+        lastTarget = hit.transform.gameObject;
+    }
+
+    void disableOutline(GameObject lastTarget)
+    {
+        if(materialRenderer == null)
+        {
+            return;
+        }
+        mats = materialRenderer.materials;
+        //Here, you have to assign with the exact order!
+        if (lastTarget.transform.CompareTag("Artere") == true)
+        {
+            mats[0] = artere;
+        }
+        //Here, you have to assign with the exact order!
+        else if (lastTarget.transform.CompareTag("Organe") == true)
+        {
+            mats[0] = organe;
+        }
+        //Here, you have to assign with the exact order!
+        else if (lastTarget.transform.CompareTag("Os") == true)
+        {
+            mats[0] = os;
+        }
+        else
+        {
+            return;
+        }
+
+        materialRenderer.materials = mats;
+        materialRenderer = null;
+
+    }
+}
---
+++ GaCha 2018 - Dissection/Assets/Arthur/suisobj.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class suisobj : MonoBehaviour {
+
+    public Transform a;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.position = a.position;
+	}
+}
