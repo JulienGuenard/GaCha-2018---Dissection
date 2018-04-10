ca663d5efa2648be7f352b06592f4697471dd492
Unknown <oncheonche@ymail.com>
2018-04-10 11:23:57 +0200

    Test de jeu + Trouvé un decal systems

---
+++ GaCha 2018 - Dissection/Assets/Arthur/Managers/Card.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour {
+    public DeathType deathType;
+    public ChallengeType ChallengeType;
+    public int wonPoints;
+    public string description;
+}
---
+++ GaCha 2018 - Dissection/Assets/Arthur/Managers/CardsManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ChallengeType { Easy, Medium, Hard };
+
+public class CardsManager : MonoBehaviour {
+
+    public List<Card> CardsList = new List<Card>();
+
+    
+}
