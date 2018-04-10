7490f18c113d013d479a85c1f365b600c0c60373
Theo Galland <tetebad@hotmail.fr>
2018-04-10 18:31:57 +0200

    commit menu du boloss

---
+++ GaCha 2018 - Dissection/Assets/Theo/Scripts/Buttons.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour {
+
+	public void MainGame() {
+		SceneManager.LoadScene ("MainGame", LoadSceneMode.Single);
+	}
+
+	public void Credits() {
+		SceneManager.LoadScene("Credits", LoadSceneMode.Single);
+	}
+
+	public void MainMenu() {
+		SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+	}
+
+	public void Commands() {
+		SceneManager.LoadScene ("Commands", LoadSceneMode.Single);
+	}
+
+	public void CardsMenu() {
+		SceneManager.LoadScene ("CardsMenu", LoadSceneMode.Single);
+	}
+
+	public void NamingMenu() {
+		SceneManager.LoadScene ("NamingMenu", LoadSceneMode.Single);
+	}
+
+	public static void CustomizationMenu() {
+		SceneManager.LoadScene ("CustomizationMenu", LoadSceneMode.Single);
+	}
+
+	public void Quit() {
+		Application.Quit();
+	}
+}
